bc06db6d16a90020101e50236fa459aaf0058936
Ciaffi-Jacob <ciaffi.jacob@gmail.com>
2023-07-01 16:45:49 -0400

    assessment 1 ArrayListUtils passed

--- Exams/java-assessment-1/src/main/java/com/zipcodewilmington/assessment1/part2/ArrayUtils.java
+++ Exams/java-assessment-1/src/main/java/com/zipcodewilmington/assessment1/part2/ArrayUtils.java
@@ -1,5 +1,8 @@
 package com.zipcodewilmington.assessment1.part2;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Created by leon on 2/16/18.
  */
@@ -11,7 +14,13 @@ public class ArrayUtils {
      * Given an array of objects, named `objectArray`, and an object `objectToCount`, return the number of times the `objectToCount` appears in the `objectArray`
      */
     public static Integer getNumberOfOccurrences(Object[] objectArray, Object objectToCount) {
-        return null;
+        Integer count = 0;
+        for (int i = 0; i < objectArray.length; i++) {
+            if(objectToCount.equals(objectArray[i])){
+                count += 1;
+            }
+        }
+        return count;
     }
 
     /**
@@ -20,8 +29,14 @@ public class ArrayUtils {
      * @return an array with identical content excluding the specified `objectToRemove`
      * Given an array of objects, name `objectArray`, and an object `objectToRemove`, return an array of objects with identical contents excluding `objectToRemove`
      */
-    public static Object[] removeValue(Object[] objectArray, Object objectToRemove) {
-        return null;
+    public static Integer[] removeValue(Integer[] objectArray, Integer objectToRemove) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        for (int i = 0; i < objectArray.length; i++) {
+            if(!(objectToRemove.equals(objectArray[i]))){
+                arrayList.add(objectArray[i]);
+            }
+        }
+        return arrayList.toArray(new Integer[arrayList.size()]);
     }
 
     /**
@@ -29,8 +44,16 @@ public class ArrayUtils {
      * @return the most frequently occurring object in the array
      * given an array of objects, named `objectArray` return the most frequently occuring object in the array
      */
-    public static Object getMostCommon(Object[] objectArray) {
-        return null;
+    public static Integer getMostCommon(Integer[] objectArray) {
+        Integer count = 0;
+        int mem = 0;
+        for (int i = 0; i < objectArray.length; i++) {
+            if(count.intValue() < getNumberOfOccurrences(objectArray, objectArray[i]).intValue()){
+                count = getNumberOfOccurrences(objectArray, objectArray[i]);
+                mem = i;
+            }
+        }
+        return objectArray[mem];
     }
 
 
@@ -39,8 +62,16 @@ public class ArrayUtils {
      * @return the least frequently occurring object in the array
      * given an array of objects, named `objectArray` return the least frequently occuring object in the array
      */
-    public static Object getLeastCommon(Object[] objectArray) {
-        return null;
+    public static Integer getLeastCommon(Integer[] objectArray) {
+        Integer count = Integer.MAX_VALUE;
+        int mem = 0;
+        for (int i = 0; i < objectArray.length; i++) {
+            if(count.intValue() > getNumberOfOccurrences(objectArray, objectArray[i]).intValue()){
+                count = getNumberOfOccurrences(objectArray, objectArray[i]);
+                mem = i;
+            }
+        }
+        return objectArray[mem];
     }
 
     /**
@@ -49,7 +80,14 @@ public class ArrayUtils {
      * @return an array containing all elements in `objectArray` and `objectArrayToAdd`
      * given two arrays `objectArray` and `objectArrayToAdd`, return an array containing all elements in `objectArray` and `objectArrayToAdd`
      */
-    public static Object[] mergeArrays(Object[] objectArray, Object[] objectArrayToAdd) {
-        return null;
+    public static Integer[] mergeArrays(Integer[] objectArray, Integer[] objectArrayToAdd) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        for (int i = 0; i < objectArray.length; i++) {
+            arrayList.add(objectArray[i]);
+        }
+        for (int i = 0; i < objectArrayToAdd.length; i++) {
+            arrayList.add(objectArrayToAdd[i]);
+        }
+        return arrayList.toArray(new Integer[objectArray.length + objectArrayToAdd.length]);
     }
 }
