bc23180f8f8df8201567346b227c5a575939d8cc
Ciaffi-Jacob <ciaffi.jacob@gmail.com>
2024-05-19 13:29:12 -0400

    StringUtilsTest done

--- Exams/java-assessment-1/src/main/java/com/zipcodewilmington/assessment1/part2/StringUtils.java
+++ Exams/java-assessment-1/src/main/java/com/zipcodewilmington/assessment1/part2/StringUtils.java
@@ -22,7 +22,7 @@ public class StringUtils {
      * given a string containing words delimited by spaces, representative of a sentence, return the first word of the sentence
      */
     public static String getFirstWord(String sentence) {
-        return null;
+        return getWords(sentence)[0];
     }
 
     /**
@@ -31,7 +31,8 @@ public class StringUtils {
      * given a string containing words delimited by spaces, representative of a sentence, return the first word with identical contents in reverse order
      */
     public static String reverseFirstWord(String sentence) {
-        return null;
+        StringBuilder reverseString = new StringBuilder(getFirstWord(sentence).toString());
+        return reverseString.reverse().toString();
     }
 
     /**
@@ -40,7 +41,12 @@ public class StringUtils {
      * given a string containing words delimited by spaces, representative of a sentence, return the first word with identical contents in reverse order with the first character capitalized
      */
     public static String reverseFirstWordThenCamelCase(String sentence) {
-        return null;
+        StringBuilder builder = new StringBuilder(reverseFirstWord(sentence));
+        char indexIUppercase = Character.toUpperCase(builder.charAt(0));
+        builder.insert(0, indexIUppercase);
+        builder.delete(1,2);
+      //  builder.insert(0, indexIUppercase);
+        return builder.toString();
     }
 
 
@@ -51,7 +57,9 @@ public class StringUtils {
      * given a string and index, return an identical string excluding the character at the specified index
      */
     public static String removeCharacterAtIndex(String str, int index) {
-        return null;
+        StringBuilder string = new StringBuilder(str);
+        string.delete(index, index+1);
+        return string.toString();
     }
 
 }
